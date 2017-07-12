0e1c9b9f9c26cdcb68f670a3cdf479de56c60200
Tomaz <TomazWang.tw@gmail.com>
2017-07-12 17:41:21 +0800

    add log

--- app/__init__.py
+++ app/__init__.py
@@ -35,6 +35,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+    print("app:callback     ", "Request body: ", body)
 
     # handle webhook body
     try:
