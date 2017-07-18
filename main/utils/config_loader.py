47fc7c796165b43288321be4b70e38cc7919448e
Tomaz <TomazWang.tw@gmail.com>
2017-07-18 13:53:24 +0800

    try to fix mongodb

--- main/utils/config_loader.py
+++ main/utils/config_loader.py
@@ -5,6 +5,7 @@ class Config:
     def __init__(self):
         self.channel_secret = os.environ.get('CHANNEL_SECRET', '')
         self.channel_access_token = os.environ.get('CHANNEL_ACCESS_TOKEN','')
+        self.mongodb_uri = os.environ.get('MONGODB_URI', '')
         self.db_uri = os.environ.get('DB_URI', '127.0.0.1')
         self.db_name = os.environ.get('DB_NAME', 'stinky-db')
 #         if 'CHANNEL' in config:
